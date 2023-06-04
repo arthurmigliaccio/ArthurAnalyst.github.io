bb8da998cade8e544e94460c2c543dde7cb63ec7
Arthur Migliaccio <129797416+arthurmigliaccio@users.noreply.github.com>
2023-06-04 20:30:47 +0100

    Add files via upload
    commit assets

---
+++ assets/js/matrix.js
@@ -0,0 +1,16 @@
+
+VANTA.NET({
+el: "#your-element-selector",
+mouseControls: true,
+touchControls: true,
+gyroControls: false,
+minHeight: 200.00,
+minWidth: 200.00,
+scale: 1.00,
+scaleMobile: 1.00,
+color: 0x#6fc3df,
+backgroundColor: 0x0,
+points: 20.00,
+maxDistance: 28.00,
+spacing: 19.00
+})
